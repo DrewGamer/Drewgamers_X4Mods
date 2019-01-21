642e19455fab2215ae4b6a0cb96606c7c5ae4baa
Andrew Ruegamer <arsailor89@yahoo.com>
2019-01-21 16:52:00 -0500

    changes to bat script

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# DMT
+# Drewgamer's X4 Mods
+Collection of mods for the game X4: Foundations which are being worked on or maintainged by Drewgamer
--- ResearchOverhaul/ResearchOverhaul_Medium/pack.bat
+++ ResearchOverhaul/ResearchOverhaul_Medium/pack.bat
@@ -1,4 +1,5 @@
 del /Q /S ..\packed\ResearchOverhaul_Medium\*
+del /Q /S ..\packed\ResearchOverhaul_Medium.zip
 rmdir /Q /S ..\packed\ResearchOverhaul_Medium
 for %%I in (.\*.cat .\*.dat) do del /Q "%%I"
 REM "..\XRCatTool.exe" -in subst_01 -out subst_01.cat -dump
@@ -8,5 +9,5 @@ mkdir ..\packed\ResearchOverhaul_Medium\ResearchOverhaul
 copy .\content.xml ..\packed\ResearchOverhaul_Medium\ResearchOverhaul\
 copy .\ResearchOverhaul.lua ..\packed\ResearchOverhaul_Medium\ResearchOverhaul\
 for %%I in (.\*.cat .\*.dat) do move "%%I" ..\packed\ResearchOverhaul_Medium\ResearchOverhaul\
-"..\7za.exe" -7z ResearchOverhaul.7z 
+"..\7z\7za.exe" a ..\packed\ResearchOverhaul_Medium.zip ..\packed\ResearchOverhaul_Medium\ResearchOverhaul\*
 pause
